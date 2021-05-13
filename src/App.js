44c155fad560369bdd0c3476b8e4df34df158c5e
liis.kilp <kilp.liis@gmail.com>
2021-05-13 17:05:18 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import './App.scss';
+import Application from './components/Application';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Infrastructure from './components/Infrastructure';
+import Intro from './components/Intro';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Header />
+      <Intro />
+      <Infrastructure />
+      <Application />
+      <Footer />
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+        Coded by <a href="https://lkilp.github.io/portfolio/">Liis Kilp</a>.
+      </div>
     </div>
   );
 }
---
+++ src/menu-items.js
@@ -0,0 +1,83 @@
+
+const menuItems = [
+    {
+        id:'product',
+        name: 'Product',
+        subs: [
+            {
+                id: 'overview',
+                name: 'Overview',
+                url: '/',
+            },
+            {
+                id: 'pricing',
+                name: 'Pricing',
+                url: '/',
+            },
+            {
+                id: 'marketplace',
+                name: 'Marketplace',
+                url: '/',
+            },
+            {
+                id: 'features',
+                name: 'Features',
+                url: '/',
+            },
+            {
+                id: 'integrations',
+                name: 'Integrations',
+                url: '/',
+            },
+        ]
+    },
+    {
+        id:'company',
+        name: 'Company',
+        subs: [
+            {
+                id: 'about',
+                name: 'About',
+                url: '/',
+            },
+            {
+                id: 'team',
+                name: 'Team',
+                url: '/',
+            },
+            {
+                id: 'blog',
+                name: 'Blog',
+                url: '/',
+            },
+            {
+                id: 'careers',
+                name: 'Careers',
+                url: '/',
+            },
+        ]
+    },
+    {
+        id:'connect',
+        name: 'Connect',
+        subs: [
+            {
+                id: 'contact',
+                name: 'Contact',
+                url: '/',
+            },
+            {
+                id: 'newsletter',
+                name: 'Newsletter',
+                url: '/',
+            },
+            {
+                id: 'linkedIn',
+                name: 'LinkedIn',
+                url: '/',
+            },
+        ]
+    },
+];
+
+export default menuItems;
